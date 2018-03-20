8761b45ac6742a330a9a9096742d92950002ed48
Andrey <andreyproskura88@gmail.com>
2018-03-20 22:39:34 +0200

    created baseViewModel

---
+++ RosterApp/RosterApp/ViewModels/BaseView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RosterApp.ViewModels
+{
+    public class BaseView : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
