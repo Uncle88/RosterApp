b026fa48f12c479d226e279b7e5c1a970ed2a1e8
Andrey <andreyproskura88@gmail.com>
2018-03-22 14:47:35 +0200

    added DatabaseService

---
+++ RosterApp/RosterApp/Services/DataBase/DataBaseService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using RosterApp.Models;
+
+namespace RosterApp.Services.DataBase
+{
+    public class DataBaseService : IDataBaseService
+    {
+        public DataBaseService()
+        {
+        }
+
+        public void DeleteItemFromDB(List<Market> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveItemToDB(List<Market> item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RosterApp/RosterApp/Services/DataBase/IDataBaseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using RosterApp.Models;
+
+namespace RosterApp.Services.DataBase
+{
+    public interface IDataBaseService
+    {
+        void SaveItemToDB(List<Market> item);
+        void DeleteItemFromDB(List<Market> item);
+    }
+}
